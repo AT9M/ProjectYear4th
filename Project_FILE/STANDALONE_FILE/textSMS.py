7f7c6fcb4a04a8eddcc94bee5ba10701fb6c838c
AT9M <46815477+AT9M@users.noreply.github.com>
2020-01-30 12:06:46 +0000

    Update textSMS.py
    change some values

--- Project_FILE/STANDALONE_FILE/textSMS.py
+++ Project_FILE/STANDALONE_FILE/textSMS.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid ="AC6a6f62dbaf928dab9d849e25d1412efb" # Put your Twilio account SID here
+account_sid ="You would like to know" # Put your Twilio account SID here
 
-auth_token ="25380b677c458cbfca4abc0eea9bdce0" # Put your auth token here
+auth_token ="Still no" # Put your auth token here
 
 client = Client(account_sid, auth_token)
 
 message = client.api.account.messages.create(
 
-to="+33617966728", # Put your cellphone number here
+to="+Do not call", # Put your cellphone number here
 
 from_="+447588675953", # Put your Twilio number here
 
